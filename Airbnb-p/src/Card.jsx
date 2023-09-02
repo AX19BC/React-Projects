21abd73653530158670ecb973e85b1d2b0d804c3
Nejc Vidmar <nejcviddmar@gmail.com>
2023-09-02 20:33:30 +0200

    hard coded cards

--- Airbnb-p/src/Card.jsx
+++ Airbnb-p/src/Card.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from 'react'; 
 
-export default function Card (){
+export default function Card (props){
     return (
-        <div className=""></div>
+        <div className="pt-[4%] pl-[9%]">
+            <div className="">
+                <div>
+                    <img src="../public/image 12.png"></img>
+                </div>
+                <div className="flex">
+                    <img className="pl-1"src="../public/Star 1.png"></img>
+                    <p className="pl-2">5.0 (6)•USA</p>
+                </div>
+                <p className="pl-1 pt-3 max-w-[300px]">Life lessons with Katie Zaferes <b>From $136</b> / person</p>
+            </div>
+        </div>
     )
 }
