62da27370af1027e8ceba94d0ad8f9e461db81cf
Nejc Vidmar <nejcviddmar@gmail.com>
2023-09-02 21:18:15 +0200

    added flexible card component

--- Airbnb-p/src/App.jsx
+++ Airbnb-p/src/App.jsx
@@ -5,15 +5,27 @@ import './App.css'
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Card from './Card'
+import Data from './data'
+import data from './data.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const cards = data.map((card) => 
+    <Card img={card.coverImg}
+          rating={card.stats.rating}
+          reviewCount={card.stats.reviewCount}
+          location={card.location}
+          title={card.title}
+          price={card.price}
+    />);
+  
 
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      {cards}
     </div>
   )
 }
--- Airbnb-p/src/Card.jsx
+++ Airbnb-p/src/Card.jsx
@@ -4,14 +4,14 @@ export default function Card (props){
     return (
         <div className="pt-[4%] pl-[9%]">
             <div className="">
-                <div>
-                    <img src="../public/image 12.png"></img>
+                <div className="w-[300px]">
+                    <img className="w-[300px] h-[400px]" src={`../public/${props.img}`}></img>
                 </div>
                 <div className="flex">
                     <img className="pl-1"src="../public/Star 1.png"></img>
-                    <p className="pl-2">5.0 (6)•USA</p>
+                    <p className="pl-2">{props.rating} ({props.reviewCount})•{props.location}</p>
                 </div>
-                <p className="pl-1 pt-3 max-w-[300px]">Life lessons with Katie Zaferes <b>From $136</b> / person</p>
+                <p className="pl-1 pt-3 max-w-[300px]">Life lessons with Katie Zaferes <b>From ${props.price}</b> / person</p>
             </div>
         </div>
     )
