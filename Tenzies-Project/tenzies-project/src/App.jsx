056e2326fa5807a7e22a3b8fdcb5647174cefa2b
Nejc Vidmar <nejcviddmar@gmail.com>
2023-09-24 16:46:03 +0200

    Logic all done. Game is working

--- Tenzies-Project/tenzies-project/src/App.jsx
+++ Tenzies-Project/tenzies-project/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Die from './Die.jsx'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   
   const [dice, setDice] = useState(allNewDice)
-  
-  
+  const [tenzies, setTenzies] = useState(false)
 
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHold)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+        }
+  }, [dice]);
+  
   function allNewDice () {
     const randomArray = []
     for (let i = 0; i < 10; i++) {
@@ -31,6 +40,10 @@ function App() {
               id: nanoid(),
             }
     }))
+    if(dice.every(die => die.isHold)) {
+      setTenzies(false)
+      setDice(allNewDice)
+    }
 }
 
   function holdDice(id) {
@@ -41,13 +54,13 @@ function App() {
     }))
 }
   
-  
   const diceElement = dice.map(element => 
     <Die value={element.value} key= {element.id} hold={element.isHold} holdDice={() => holdDice(element.id)}/>)
 
   return (
     <>
-      <div className="w-[100%] h-[100%] flex justify-center p-[60px] pt-[80px] bg-gradient-to-r from-zinc-700 to-indigo-900"> 
+      <div className="w-[100%] h-[auto] flex justify-center pt-[80px] bg-gradient-to-r from-zinc-700 to-indigo-900"> 
+      {tenzies === true ? <Confetti width={window.innerWidth} height={window.innerHeight} /> : null}
         <div className="min-h-[500px] w-[500px] bg-gray-100 rounded-xl p-[70px] pt-[70px]">
 
             <div className="Titles pb-4">
@@ -60,7 +73,8 @@ function App() {
             </div>
 
             <div className="button pt-[40px]">
-              <button onClick = {rollDice}  className="bg-[#3b3452] hover:bg-gradient-to-br from-zinc-700 to-indigo-900 text-white text-xl font-bold rounded-lg p-4 w-[100%] h-[50px] flex justify-center items-center tracking-[0.2em]">Roll</button>
+              <button onClick = {rollDice} className="bg-[#3b3452] hover:bg-gradient-to-br from-zinc-700 to-indigo-900 text-white text-xl font-bold rounded-lg p-4 w-[100%] h-[50px] flex justify-center items-center tracking-[0.2em]">
+                {tenzies ===true ? "New game" : "Roll"}</button>
             </div>
         </div>
       </div>
