63a019712d7bd6fef6d1eb376bf2de2b780abdb5
Nejc Vidmar <nejcviddmar@gmail.com>
2023-09-24 15:28:48 +0200

    tenzies project add stay active button and numbers of this button dont change.

--- Tenzies-Project/tenzies-project/src/App.jsx
+++ Tenzies-Project/tenzies-project/src/App.jsx
@@ -1,20 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Die from './Die.jsx'
+import {nanoid} from 'nanoid'
 
 function App() {
   
+  const [dice, setDice] = useState(allNewDice)
   
+  
+
   function allNewDice () {
     const randomArray = []
     for (let i = 0; i < 10; i++) {
-      randomArray[i]= (Math.floor(Math.random() * 6) + 1)
+      randomArray[i]= {
+        value: (Math.floor(Math.random() * 6) + 1), 
+        isHold: false,
+        id: nanoid(),
+      }
     }
     return randomArray
   }
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isHold ? 
+            die :
+            {
+              value: (Math.floor(Math.random() * 6) + 1), 
+              isHold: false,
+              id: nanoid(),
+            }
+    }))
+}
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHold: !die.isHold} :
+            die
+    }))
+}
   
+  
+  const diceElement = dice.map(element => 
+    <Die value={element.value} key= {element.id} hold={element.isHold} holdDice={() => holdDice(element.id)}/>)
 
   return (
     <>
@@ -27,20 +56,11 @@ function App() {
             </div>
 
             <div className="boxes grid grid-rows-2 grid-flow-col gap-4 pt-[20px]">
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
-                <Die value ="1"/>
+               {diceElement}
             </div>
 
             <div className="button pt-[40px]">
-              <button className="bg-[#3b3452] hover:bg-gradient-to-br from-zinc-700 to-indigo-900 text-white text-xl font-bold rounded-lg p-4 w-[100%] h-[50px] flex justify-center items-center tracking-[0.2em]">Roll</button>
+              <button onClick = {rollDice}  className="bg-[#3b3452] hover:bg-gradient-to-br from-zinc-700 to-indigo-900 text-white text-xl font-bold rounded-lg p-4 w-[100%] h-[50px] flex justify-center items-center tracking-[0.2em]">Roll</button>
             </div>
         </div>
       </div>
