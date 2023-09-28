bf6b140001040796e982347b352bf19a34fd462a
Nejc Vidmar <nejcviddmar@gmail.com>
2023-09-28 20:37:16 +0200

    It is all done

--- Kviz-react/src/Question.jsx
+++ Kviz-react/src/Question.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import { decode } from 'html-entities';
 
-export default function Question (props){
-    const { incorrect_answers, correct_answer } = props.obj;
+export default function Question({ obj, selectedAnswer, onSelectAnswer, isAnswersChecked }) {
+    const { all_answers, correct_answer } = obj;
 
-    // Combine incorrect and correct answers and shuffle
-    const answers = [...incorrect_answers];
-    const randomIndex = Math.floor(Math.random() * (answers.length + 1));
-    answers.splice(randomIndex, 0, correct_answer);
-
-    // Map over answers to create array with keys
-    const arrayWithKeys = answers.map((answer, index) => {
-        return { id: index.toString(), value: answer, isSelected: false };
-    });
-
-    return(
+    return (
         <div className='question w-[50%] pt-[50px] pb-[10px] pl-7 text-left'>
             <h1 className="text-[20px] font-bold tracking-[0.1em] bg-gradient-to-br from-blue-900 via-indigo-900 to-blue-700 bg-clip-text text-transparent">
-                {decode(props.obj.question)}
+                {decode(obj.question)}
             </h1>
             <div className="grid-rows-1 pl-0 p-6  w-auto md:grid-rows-2 ">
-                {arrayWithKeys.map(answer =>
-                     <button key={answer.id} className='p-3 text-lg font-bold text-indigo-900 h-[50px] mr-[20px] mt-6 tracking-[0.2em]
-                     rounded-lg shadow-md border border-indigo-900 hover:bg-indigo-100'>{decode(answer.value)}</button>
-                     )}
+                {all_answers.map((answer, index) => (
+                    <button
+                        key={index}
+                        onClick={() => !isAnswersChecked && onSelectAnswer(answer)}
+                        className={`p-3 text-lg font-bold text-indigo-900 h-[50px] mr-[20px] mt-6 tracking-[0.2em] rounded-lg shadow-md border border-indigo-900 
+                        ${isAnswersChecked ? (answer === correct_answer ? 'bg-green-400' : (answer === selectedAnswer ? 'bg-red-400' : '')) : (answer === selectedAnswer ? 'bg-violet-300' : 'hover:bg-violet-300')}`}
+                    >
+                        {decode(answer)}
+                    </button>
+                ))}
             </div>
             <hr></hr>
         </div>
